003064a6ba1766c4f8084f2304c775381522eb3a
Mike Shelton <mshelton@visitpay.com>
2020-01-30 15:26:03 -0700

    Removed the ability to have multiple tools in a single script call.  Makes the code too complex, easy enough to call the script multiple times.  Also added comments to indicate where a programmer would need to add code in order to add another tool module to this system.

--- Python/ArgParser.py
+++ Python/ArgParser.py
@@ -1,9 +1,9 @@
 class Parser:
     def __init__(self, arg_list):
-        self.function_list = []
+        self.function_name = ""
         self.arg_pairs = []
         self.arg_names = []
         return
 
-    def get_function_names(self):
-        return self.function_list
+    def get_function_name(self):
+        return self.function_name
--- Python/Automator.py
+++ Python/Automator.py
@@ -4,38 +4,37 @@ import ArgParser as ap
 import ErrorHandling as err
 
 class Automator:
-    FNAME = os.path.basename(sys.argv[0])
-    USAGE = f"usage: {FNAME} -tool <tool_name> [-parm1 [<parm1_value>] .. -parmN " \
+    FILE_NAME = os.path.basename(sys.argv[0])
+    # ADDTOOL:  Add an example of using your tool with any required or optional parameters.
+    #   Each new tool usage line should start with f"\t{FILE_NAME} -tool <your_tool_name> ..
+    USAGE = f"usage: {FILE_NAME} -tool <tool_name> [-parm1 [<parm1_value>] .. -parmN " \
             "[<parmN_value>]]\nExamples:\n" \
-            f"\t{FNAME} -tool Sample -basedir <path> -filename <name> -version <file_version> -start " \
+            f"\t{FILE_NAME} -tool Sample -basedir <path> -filename <name> -version <file_version> -start " \
             f"<start_date> -end <end_date>"
 
     def __init__(self):
-        # Create the arg parser object with the sys.args list of arguments
-        self.arg_parser = None
+        # Create the arg parser object with the sys.args list of arguments.  This will be used by each tool
+        # module to generate the options and data it will need.
+        self.arg_parser = ap.Parser(sys.argv)
+        self.tool_name = self.arg_parser.get_function_name()
         return
 
-    def init(self, arg_list):
-        self.arg_parser = ap.Parser(arg_list)
-        self.tool_list = self.arg_parser.get_function_names()
-
     def run(self):
-        if len(self.tool_list) == 0:
-            err.error_abort("No tools chosen", Automator.USAGE)
+        # ADDTOOL: This is where you will add your tool creation and call for a new tool.
+        #   Follow the "Sample" example (used for unit test of framework).  Import the module file,
+        #   create the tool object and pass it the arg_parser, then run the tool.
+        if self.tool_name == "Sample":
+            import SampleTool as st
+            tool_object = st.Sample_Tool(self.arg_parser)
+            tool_object.run()
+        else:
+            err.error_continue(f"'{self.tool_name}' invalid tool")
 
-        for tool in self.tool_list:
-            if tool['name'] == "Sample":
-                import SampleTool as st
-                tool_object = st.Sample_Tool(self.arg_parser)
-                tool_object.run()
-            else:
-                err.error_continue(f"'{tool.name}' invalid tool")
         return
 
 
 def main():
     the_automator = Automator()
-    the_automator.init(sys.argv)
     the_automator.run()
 
 
